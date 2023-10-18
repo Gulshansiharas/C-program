0aa42b393557b0a1b14ff67e7b49430ddc1737b3
Gulshansiharas <144225879+Gulshansiharas@users.noreply.github.com>
2023-10-18 07:38:39 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,74 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+// int main()
+// {
+//     int n=12;
+//     for (int i=0;i<n;++i){
+//     printf("Hello World\n");}
+
+//     return 0;
+// // }
+// int main()
+// {
+//     int i=0;
+//     printf("The listed numbers are:\n");
+//     for(i=1;i<=10;i++){
+//         printf("%d\n",i);
+        
+//     }
+//     return 0;
+// }
+// int main(){
+//     int i=3;
+// while(i<10){
+//     printf("Hello ,World\n");
+//     i++;}
+//     return 0;
+    
+// }
+// int main(){
+//     int i=0;
+//     do{
+//         printf("Hello,World\n");
+//         i++;
+//     }while(i<5);
+//     return 0;
+// }
+// int main(){
+//     int i =2;
+//     if(i<15){
+//         printf("i is smaller than 15");
+//     }
+//     else{
+//         printf("i is greater than 15");
+//     }
+//     return 0;}
+// int main(){
+//     int i=19;
+//     if(i==19){
+//         if(i<15){
+//             printf("i is smaller than 15");}
+//         if(i<12){
+//             printf("i is smaller than 12");}
+//         else{
+//             printf("i is greater than 15");
+//         }
+//         return 0;
+        
+//     }
+// int main(){
+//     int i=20;
+//     if(i==10){printf("i is 10");}
+//     else if(i==15){printf("i is 14");
+// }else{printf("i is not present");}
+// return 0;}
+// }
+
